0b15abecf5fa168a2f0aa997ec2c6b15a07f7b41
Peter Hultqvist <hultqvist@silentorbit.com>
2019-03-13 23:35:36 +0100

    Force all microphone levels to 100%

---
+++ FixVolume/NotifyIconContext.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace SilentOrbit.FixVolume
+{
+    internal class NotifyIconContext : ApplicationContext
+    {
+        readonly NotifyIcon trayIcon;
+
+        readonly Icon Red = GenerateIcon(Color.Red);
+        readonly Icon Blue = GenerateIcon(Color.Blue);
+        readonly Icon Yellow = GenerateIcon(Color.Yellow);
+
+        public NotifyIconContext()
+        {
+            trayIcon = new NotifyIcon()
+            {
+                Icon = GenerateIcon(Color.Red),
+                ContextMenu = new ContextMenu(new MenuItem[] { new MenuItem("Exit", Exit) }),
+                Visible = true
+            };
+        }
+
+        internal void Exit(object sender, EventArgs e)
+        {
+            trayIcon.Visible = false;
+            Application.Exit();
+        }
+
+        public void Error(int timeout, string title, string message)
+        {
+            trayIcon.ShowBalloonTip(timeout, title, message, ToolTipIcon.Error);
+            trayIcon.Text = title + ": " + message;
+            trayIcon.Icon = Red;
+        }
+
+        public void Info(int timeout, string title, string message)
+        {
+            trayIcon.ShowBalloonTip(timeout, title, message, ToolTipIcon.Info);
+            trayIcon.Text = title + ": " + message;
+            trayIcon.Icon = Blue;
+        }
+
+        public void Warning(int timeout, string title, string message)
+        {
+            trayIcon.ShowBalloonTip(timeout, title, message, ToolTipIcon.Warning);
+            trayIcon.Text = title + ": " + message;
+            trayIcon.Icon = Yellow;
+        }
+
+        static Icon GenerateIcon(Color color)
+        {
+            using (Bitmap b = new Bitmap(32, 32))
+            {
+                using (var g = Graphics.FromImage(b))
+                {
+                    g.Clear(color);
+                }
+
+                return Icon.FromHandle(b.GetHicon());
+            }
+        }
+    }
+}
---
+++ FixVolume/Program.cs
@@ -0,0 +1,78 @@
+﻿using AudioSwitcher.AudioApi.CoreAudio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SilentOrbit.FixVolume
+{
+    static class Program
+    {
+        static NotifyIconContext context;
+
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            context = new NotifyIconContext();
+
+            Thread t = new Thread(Run);
+            t.IsBackground = true;
+            t.Start();
+
+            Application.Run(context);
+        }
+
+        private static void Run()
+        {
+            try
+            {
+                context.Error(1000, "Loading", "Starting controller");
+                Console.WriteLine("Loading controller");
+
+                var ctrl = new CoreAudioController();
+
+                while (true)
+                {
+                    //Communication capture
+                    {
+                        var dev = ctrl.GetDefaultDevice(AudioSwitcher.AudioApi.DeviceType.Capture, AudioSwitcher.AudioApi.Role.Communications);
+                        FixVolume(dev);
+                    }
+                    foreach (var dev in ctrl.GetDevices())
+                    {
+                        if (dev.State != AudioSwitcher.AudioApi.DeviceState.Active)
+                            continue;
+
+                        if ((dev.DeviceType & AudioSwitcher.AudioApi.DeviceType.Capture) != 0)
+                        {
+                            FixVolume(dev);
+                        }
+                    }
+
+                    Thread.Sleep(1000);
+                }
+            }
+            catch (Exception ex)
+            {
+                context.Error(60000, ex.GetType().Name, ex.Message);
+                Thread.Sleep(60000);
+                context.Exit(null, null);
+            }
+        }
+
+        private static void FixVolume(CoreAudioDevice dev)
+        {
+            if (dev.Volume != 100)
+            {
+                dev.Volume = 100;
+                Console.WriteLine(DateTime.Now + " Forced Volume: 100");
+                context.Warning(5000, "Forced volume 100%", dev.FullName);
+            }
+        }
+    }
+}
